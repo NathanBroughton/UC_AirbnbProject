7cb778a031c5eae98873ab8720eacce21ee8e107
NathanBroughton <natbro@jjtn.nl>
2023-01-10 23:46:53 +0100

    Add files via upload

--- Methods.py
+++ Methods.py
@@ -7,21 +7,23 @@ Created on Mon Nov 28 14:10:10 2022
 
 import pandas as pd
 import statsmodels.api as sm
-from statsmodels.stats.outliers_influence import variance_inflation_factor
 import numpy as np
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 from sklearn import svm, metrics
-from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import VotingClassifier
 from imblearn.over_sampling import RandomOverSampler, SMOTE
 from imblearn.under_sampling import RandomUnderSampler
-from matplotlib.ticker import ScalarFormatter
 import os
 import time
-import matplotlib.pyplot as plt
 from collections import Counter
+import json
+import math
+from tqdm import tqdm
+from esda.moran import Moran
+from libpysal.weights import full2W
 
 
 pd.options.mode.chained_assignment = None
@@ -29,6 +31,7 @@ pd.options.mode.chained_assignment = None
 ROS = RandomOverSampler()
 RUS = RandomUnderSampler()
 SM = SMOTE()
+n = 1
 
 
 #Initiate training and test set
@@ -42,35 +45,28 @@ for file in os.listdir('./Data3/Test'):
         path = os.path.join('./Data3/Test',file)
         df = pd.read_csv(path,index_col=None,header = 0)
         test_set.append(df)
+        # If no space in city name
+        city = str(file[:-4])
+        # If space in city name
+        # city = "New York"
         
 df_train = pd.concat(train_set,axis=0,ignore_index=True)
 df_test = pd.concat(test_set,axis=0,ignore_index=True)
 
-print(df_train.shape)
-print(df_test.shape)
 df_all = pd.concat([df_train,df_test],axis=0,ignore_index=True)
-print(df_all.shape)
 train_shape = df_train.shape
-print(train_shape[0])
+
+tracts = list(df_test["NAME"])
+
+#Data Description
+df_all = df_all.drop(['Unnamed: 0','state','county','tract','Tracts'],axis=1)
+print(df_all.info())
 
 #Plot penetration count histogram
 Y = df_all['Airbnb penetration']
 w = Counter(Y)
-print(len(w))
-fig, ax = plt.subplots()
-plt.hist(Y**(1/2), bins=40, edgecolor='black', linewidth=0.5)
-# plt.bar(w.keys(),w.values())
-plt.xlabel(r"$\sqrt{bnb\_penetration}$")
-plt.ylabel("count")
-plt.title("Frequency distribution of Airbnb penetration")
-# plt.xscale('log')
-plt.yscale('log')
-for axis in [ax.xaxis, ax.yaxis]:
-    axis.set_major_formatter(ScalarFormatter())
-plt.show()
 
 #Convert penetration number to class
-print(df_all.nlargest(int(0.1*len(df_all)),['Airbnb penetration']).index)
 df_all.to_csv('df_all.csv')
 smallest = df_all.nsmallest(int(0.7*len(df_all)),['Airbnb penetration']).index
 largest = df_all.nlargest(int(0.05*len(df_all)),['Airbnb penetration']).index
@@ -78,11 +74,9 @@ largest = df_all.nlargest(int(0.05*len(df_all)),['Airbnb penetration']).index
 
 df_all['Airbnb penetration_class'] = [1]*len(df_all)
 for i in smallest:
-    #print(df_all['Airbnb penetration_class'][i])
     df_all['Airbnb penetration_class'][i] = 0
 
 for i in largest:
-    #print(df_all['Airbnb penetration_class'][i])
     df_all['Airbnb penetration_class'][i] = 2
 
 #Start explanotary analysis
@@ -93,20 +87,29 @@ X = df_test[['Population Density','Race Diversity Index','Income Diversity Index
 
 y = df_all['Airbnb penetration_class']
 
-#Adjust skewness
+print("Skewness before")
+print(X.skew(axis=0))
+
+#Adjust skewness print("Skewness before")
+# X['Population Density'] = X['Population Density']**(1/2)
+# X['Race Diversity Index'] = X['Race Diversity Index']**(1/2)
+# X['Income Diversity Index'] = X['Income Diversity Index']**(1/2)
+# X['Bohemian Index'] = X['Bohemian Index']**(1/2)
+# X['Talent Index'] = X['Talent Index']**(1/2)
+# X['Proportion of Young People'] = X['Proportion of Young People']**(1/2)
+# X['Unemployment Ratio'] = X['Unemployment Ratio']**(1/2)
+# X['Poverty by Income Percentage'] = X['Poverty by Income Percentage']**(1/2)
+# X['Median Household Income'] = X['Median Household Income']**(1/2)
+# X['Median Household Value'] = X['Median Household Value']**(1/2)
+# X['Proportion of Owner Occupied Residences'] = X['Proportion of Owner Occupied Residences']**(1/2)
 X['Number of Hotels'] = X['Number of Hotels']**(1/2)
-X['Population Density'] = X['Population Density']**(1/2)
+# X['Bus Stops'] = X['Bus Stops']**(1/2)
 X['Point of Interests'] = X['Point of Interests']**(1/2)
-#X['Distance to Center'] = X['Distance to Center']**(1/2)
+# X['Distance to Center'] = X['Distance to Center']**(1/2)
 X['Airbnb penetration'] = X['Airbnb penetration']**(1/2)
 
-#X['Number of Hotels'] = np.log(X['Number of Hotels'].replace(0, np.nan))
-#X['Distance to Center'] = np.log(X['Distance to Center'])
-#X['Point of Interests'] = np.log(X['Point of Interests'].replace(0, np.nan))
-#X['Number of Hotels'] = X['Number of Hotels'].replace(np.nan,0)
-#X['Point of Interests'] = X['Point of Interests'].replace(np.nan,0)
 
-
+print("Skewness after")
 print(X.skew(axis=0))
 #Normalise data
 X = ((X-X.mean())/X.std())
@@ -129,12 +132,48 @@ model = sm.OLS(Y,x).fit()
 predictions = model.predict(x)
 print(model.summary())
 
-#Classification
+# Calculate Moran's I
+with open('coordinates_center_per_tract.txt', 'r') as f:
+    coordinates_center = json.loads(f.read())
+tracts_coordinates = coordinates_center[city]
+
+# Remove tracts which are not in csv city file
+tracts_dictionary = list(tracts_coordinates.keys())
+for tract in tracts_dictionary:
+    if tract not in tracts:
+        tracts_coordinates.pop(tract, None)
+
+connectivity_matrix = np.empty((len(tracts_coordinates)+1, len(tracts_coordinates)+1), dtype=object)
+connectivity_matrix[1:,0] = list(tracts_coordinates.keys())
+connectivity_matrix[0,1:] = list(tracts_coordinates.keys())
+for i in tqdm(range(1, len(tracts_coordinates)+1)):
+    for j in range(1, len(tracts_coordinates)+1):
+        if i == j:
+            connectivity_matrix[i,j] = 0
+        elif connectivity_matrix[i,j] is None:
+            # Calculate inverse Euclidean distance
+            euclidean_distance = 1 / math.dist(tracts_coordinates[connectivity_matrix[i,0]], tracts_coordinates[connectivity_matrix[0,j]])
+            connectivity_matrix[i,j] = euclidean_distance
+            connectivity_matrix[j,i] = euclidean_distance
+
+geographical = ['Distance to Center','Point of Interests', 'Number of Hotels', 'Bus Stops', 'Population Density']
+
+y_features = X[geographical]
+for feature in geographical:
+    mean_feature = X[feature].mean()
+    y_features[feature] -= mean_feature
+cross_product = np.prod(y_features, axis=1)
+moran = Moran(cross_product, full2W(connectivity_matrix[1:,1:]))
+print("Moran's I")
+print(":", moran.I)
+
+#Classification (Comment to adjust for skewness)
     
 df_all['Number of Hotels'] = df_all['Number of Hotels']**(1/2)
-df_all['Population Density'] = df_all['Population Density']**(1/2)
+#df_all['Population Density'] = df_all['Population Density']**(1/2)
+#df_all['Proportion of Owner Occupied Residences'] = df_all['Proportion of Owner Occupied Residences']**(1/2)
+#df_all['Bus Stops'] = df_all['Bus Stops']**(1/2)
 df_all['Point of Interests'] = df_all['Point of Interests']**(1/2)
-#X['Distance to Center'] = X['Distance to Center']**(1/2)
 df_all['Airbnb penetration'] = df_all['Airbnb penetration']**(1/2)
 
 #Paper sets
@@ -143,30 +182,7 @@ set0 = ['Population Density','Bohemian Index', \
         ,'Median Household Income','Median Household Value' \
         ,'Point of Interests','Distance to Center']
     
-set1 = ['Population Density','Race Diversity Index','Income Diversity Index', \
-        'Proportion of Young People','Unemployment Ratio','Poverty by Income Percentage' \
-        ,'Proportion of Owner Occupied Residences' \
-        ,'Number of Hotels','Bus Stops','Point of Interests','Distance to Center']
-    
-set2 = ['Population Density','Race Diversity Index','Income Diversity Index', \
-        'Talent Index','Proportion of Young People','Unemployment Ratio' \
-        ,'Proportion of Owner Occupied Residences' \
-        ,'Number of Hotels','Bus Stops','Point of Interests','Distance to Center']
-    
-set3 = ['Population Density','Race Diversity Index','Income Diversity Index', \
-        'Proportion of Young People','Unemployment Ratio' \
-        ,'Median Household Income','Proportion of Owner Occupied Residences' \
-        ,'Number of Hotels','Bus Stops','Point of Interests','Distance to Center']
-    
-set4 = ['Population Density','Race Diversity Index','Income Diversity Index','Bohemian Index', \
-        'Proportion of Young People','Unemployment Ratio' \
-        ,'Proportion of Owner Occupied Residences' \
-        ,'Number of Hotels','Bus Stops','Point of Interests','Distance to Center']
-    
-set5 = ['Population Density','Race Diversity Index','Income Diversity Index', \
-        'Proportion of Young People','Unemployment Ratio' \
-        ,'Median Household Value','Proportion of Owner Occupied Residences' \
-        ,'Number of Hotels','Bus Stops','Point of Interests','Distance to Center']
+set1 = ['Population Density','Bohemian Index','Number of Hotels','Bus Stops','Distance to Center']
 
 X_train = df_all[set0][:train_shape[0]]
     
@@ -179,16 +195,15 @@ y_test = df_all['Airbnb penetration_class'][train_shape[0]:]
 X_train = ((X_train-X_train.mean())/X_train.std())
 X_test = ((X_test-X_test.mean())/X_test.std())
 
-print(X_train.head())
-
-print(X_train.shape)
-print(y_train.shape)
-
-X_train1, y_train1 = RUS.fit_resample(X_train,y_train)
-X_train2, y_train2 = RUS.fit_resample(X_train,y_train)
-X_train3, y_train3 = RUS.fit_resample(X_train,y_train)
-X_train4, y_train4 = RUS.fit_resample(X_train,y_train)
-X_train5, y_train5 = RUS.fit_resample(X_train,y_train)
+Train = []
+for i in range(n): #(Comment for US, SM or nothing)
+    #X_train_app, y_train_app = X_train,y_train
+    X_train_app, y_train_app = RUS.fit_resample(X_train,y_train)
+    #X_train_app, y_train_app = SM.fit_resample(X_train,y_train)
+    Train.append(X_train_app)
+    Train.append(y_train_app)
+#X_train, y_train = RUS.fit_resample(X_train,y_train)
+#X_train, y_train = SM.fit_resample(X_train,y_train)
 
 def evaluate(cm):
     #Low
@@ -224,117 +239,47 @@ def evaluate(cm):
     
     return WA_low, WA_med, WA_high
 
+def run_model(model):
+    #for i in range(n):
+    predictions = []
+    i = 0
+    while i < n*2:
+        clf = model.fit(Train[i],Train[i+1])
+        y_pred = clf.predict(X_test)
+        predictions.append(y_pred)
+        i += 2
+    y_pred = []
+    for i in range(len(predictions[0])):
+        y = []
+        for j in range(n):
+            y.append(predictions[j][i])
+        y_pred.append(max(set(y),key = y.count))
+    cm = metrics.confusion_matrix(y_test, y_pred)
+    l,m,h = evaluate(cm)   
+    
+    return l,m,h
+
 time1 = time.time()
 
-clf1 = svm.SVC().fit(X_train1,y_train1)
-clf2 = svm.SVC().fit(X_train2,y_train2)
-clf3 = svm.SVC().fit(X_train3,y_train3)
-clf4 = svm.SVC().fit(X_train4,y_train4)
-clf5 = svm.SVC().fit(X_train5,y_train5)
-#clf = OneVsRestClassifier(svm.SVC()).fit(X_train,y_train)
-y_pred1 = clf1.predict(X_test)
-y_pred2 = clf2.predict(X_test)
-y_pred3 = clf3.predict(X_test)
-y_pred4 = clf3.predict(X_test)
-y_pred5 = clf3.predict(X_test)
-y_pred = []
-#class_0, class_1, class_2 = 0
-for i in range(len(y_pred1)):
-    y = [y_pred1[i],y_pred2[i],y_pred3[i],y_pred4[i],y_pred5[i]]
-    #print(y_pred1[i],y_pred2[i],y_pred3[i])
-    #print(max(set(y),key = y.count))
-    y_pred.append(max(set(y),key = y.count))
-    
-#print(metrics.accuracy_score(y_test,y_pred))
+l,m,h = run_model(svm.SVC())
 
-cm = metrics.confusion_matrix(y_test, y_pred)
-l,m,h = evaluate(cm)
-
-
-print(metrics.confusion_matrix(y_test, y_pred))
 print("SVM Accuracy:", (l+m+h)/3)
 print("SVM took:", time.time() - time1)
 
 time2 = time.time()
-
-clf1 = LogisticRegression(solver='liblinear',random_state=0).fit(X_train1,y_train1)
-clf2 = LogisticRegression(solver='liblinear',random_state=0).fit(X_train2,y_train2)
-clf3 = LogisticRegression(solver='liblinear',random_state=0).fit(X_train3,y_train3)
-clf4 = LogisticRegression(solver='liblinear',random_state=0).fit(X_train4,y_train4)
-clf5 = LogisticRegression(solver='liblinear',random_state=0).fit(X_train5,y_train5)
-
-y_pred1 = clf1.predict(X_test)
-y_pred2 = clf2.predict(X_test)
-y_pred3 = clf3.predict(X_test)
-y_pred4 = clf4.predict(X_test)
-y_pred5 = clf5.predict(X_test)
-
-y_pred = []
-#class_0, class_1, class_2 = 0
-for i in range(len(y_pred1)):
-    y = [y_pred1[i],y_pred2[i],y_pred3[i],y_pred4[i],y_pred5[i]]
-    #print(y_pred1[i],y_pred2[i],y_pred3[i])
-    #print(max(set(y),key = y.count))
-    y_pred.append(max(set(y),key = y.count))
-
-cm = metrics.confusion_matrix(y_test, y_pred)
-l,m,h = evaluate(cm)
+l,m,h = run_model(LogisticRegression(solver='liblinear',random_state=0))
 
 print("Logistic Accuracy:", (l+m+h)/3)
 print("Logistic took:", time.time() - time2)
 
 time3 = time.time()
-
-clf1 = RandomForestClassifier(random_state=42).fit(X_train1,y_train1)
-clf2 = RandomForestClassifier(random_state=42).fit(X_train2,y_train2)
-clf3 = RandomForestClassifier(random_state=42).fit(X_train3,y_train3)
-clf4 = RandomForestClassifier(random_state=42).fit(X_train4,y_train4)
-clf5 = RandomForestClassifier(random_state=42).fit(X_train5,y_train5)
-
-y_pred1 = clf1.predict(X_test)
-y_pred2 = clf2.predict(X_test)
-y_pred3 = clf3.predict(X_test)
-y_pred4 = clf4.predict(X_test)
-y_pred5 = clf5.predict(X_test)
-
-y_pred = []
-#class_0, class_1, class_2 = 0
-for i in range(len(y_pred1)):
-    y = [y_pred1[i],y_pred2[i],y_pred3[i],y_pred4[i],y_pred5[i]]
-    #print(y_pred1[i],y_pred2[i],y_pred3[i])
-    #print(max(set(y),key = y.count))
-    y_pred.append(max(set(y),key = y.count))
-
-cm = metrics.confusion_matrix(y_test, y_pred)
-l,m,h = evaluate(cm)
+l,m,h = run_model(RandomForestClassifier(random_state=42))
 
 print("Random Forest Accuracy",(l+m+h)/3)
 print("Random Forest took:", time.time() - time3)
 
 time4 = time.time()
-
-clf1 = GaussianNB().fit(X_train1,y_train1)
-clf2 = GaussianNB().fit(X_train2,y_train2)
-clf3 = GaussianNB().fit(X_train3,y_train3)
-clf4 = GaussianNB().fit(X_train4,y_train4)
-clf5 = GaussianNB().fit(X_train5,y_train5)
-
-y_pred1 = clf1.predict(X_test)
-y_pred2 = clf2.predict(X_test)
-y_pred3 = clf3.predict(X_test)
-y_pred4 = clf4.predict(X_test)
-y_pred5 = clf5.predict(X_test)
-
-y_pred = []
-#class_0, class_1, class_2 = 0
-for i in range(len(y_pred1)):
-    y = [y_pred1[i],y_pred2[i],y_pred3[i],y_pred4[i],y_pred5[i]]
-    #print(y_pred1[i],y_pred2[i],y_pred3[i])
-    #print(max(set(y),key = y.count))
-    y_pred.append(max(set(y),key = y.count))
-
-cm = metrics.confusion_matrix(y_test, y_pred)
-l,m,h = evaluate(cm)
+l,m,h = run_model(GaussianNB())
 
 print("Naive Bayes Accuracy", (l+m+h)/3)
 print("Naive Bayes took:", time.time() - time4)
